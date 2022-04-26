9c89747e12f51d00a0ee393c22f95dbae778fcda
chiara <chiara.babiak@gmail.com>
2022-04-26 18:34:05 +0200

    get request for map api

--- src/main/java/com/example/tour_planner/utils/windows/TourForm.java
+++ src/main/java/com/example/tour_planner/utils/windows/TourForm.java
@@ -17,6 +17,16 @@ import javafx.stage.Stage;
 // Tour class
 import com.example.tour_planner.model.Tour;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+//JSONObject
+import org.json.simple.JSONObject;
+
 public class TourForm {
 
     //build a new scene to open as a pop up form
@@ -90,12 +100,47 @@ public class TourForm {
             // send strings as get parameters to the map api
             // for the get request u need the key ( I created an account on the map api website ):
             // key = FVrDjDoWMVJKy6xoLfkNOVoafr6Z7XoP
+            try {
+                sendRequest(start, end, transport);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
 
             // create Tour Object and create Request to add a new Tour to DB
         });
 
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void sendRequest(String start, String end, String transport) throws IOException {
+        // URL for the MAP API
+        String url = "https://www.mapquestapi.com/directions/v2/route?key=6Sl7sHB1l3EjHP83Jftbgz9uffLAlMXx&from="+start+"&to="+end+"&transportMode="+transport+"&routeType="+"";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        // set it as a GET request
+        con.setRequestMethod("GET");
+        //add request header
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        int responseCode = con.getResponseCode();
+
+        // add that to the Logger instead of a system out print
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+
+        in.close();
+
+        // get the Response => response is a JSON object
     }
 
 }

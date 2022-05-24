88837351e2933583ea80664982a2fc105f229c04
chiara <chiara.babiak@gmail.com>
2022-05-24 12:41:08 +0200

    starting to implement business layer

--- src/main/java/com/example/tour_planner/layers/data/TourDao.java
+++ src/main/java/com/example/tour_planner/layers/data/TourDao.java
@@ -1,4 +1,13 @@
+// business layer for the tour component
+
 package com.example.tour_planner.layers.data;
 
+import com.example.tour_planner.layers.model.Tour;
+
 public interface TourDao {
+    void createTour(Tour tour);
+    void deleteTour(String tour_name);
+    void modifyTour(String old_name, Tour tour);
+
+
 }
--- src/main/java/com/example/tour_planner/layers/data/TourDaoImpl.java
+++ src/main/java/com/example/tour_planner/layers/data/TourDaoImpl.java
@@ -1,4 +1,21 @@
 package com.example.tour_planner.layers.data;
 
-public class TourDaoImpl {
+import com.example.tour_planner.layers.model.Tour;
+
+public class TourDaoImpl implements TourDao {
+
+    @Override
+    public void createTour(Tour tour) {
+
+    }
+
+    @Override
+    public void deleteTour(String tour_name) {
+
+    }
+
+    @Override
+    public void modifyTour(String old_name, Tour tour) {
+
+    }
 }

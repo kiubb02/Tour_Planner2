c52c59a9d8253108b9278cfcb25186c347290712
chiara <chiara.babiak@gmail.com>
2022-05-28 21:50:51 +0200

    started unit tests

---
+++ src/main/test/com/example/tour_planner/layers/business/TourServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.example.tour_planner.layers.business;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TourServiceImplTest {
+
+    @Test
+    void errorMessage() {
+    }
+
+    @Test
+    void validateInput() {
+    }
+
+    @Test
+    void validateTitle() {
+    }
+
+    @Test
+    void validateInputEdit() {
+    }
+
+    @Test
+    void calulateChildfriendl() {
+    }
+
+    @Test
+    void calculatePopularity() {
+    }
+
+    @Test
+    void importTour() {
+    }
+
+    @Test
+    void exportTour() {
+    }
+
+    @Test
+    void createTour() {
+    }
+
+    @Test
+    void generateTourReport() {
+    }
+
+    @Test
+    void generateSummary() {
+    }
+}
